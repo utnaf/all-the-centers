00da07838a8528aa79cd0082a144699d89149ec2
utnaf <utnaf.dev@gmail.com>
2018-03-07 14:00:39 +0100

    fix express server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static('public'));
 
 app.listen(80, function () {
-  console.log('Listening on port 80');
+  console.log('Listening on port ' + port);
 });
