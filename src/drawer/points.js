f7ae8c5b0971bd4ee6163cee41ebea03e5f8ac96
dfantuzzi <davide.fantuzzi@trivago.com>
2018-05-03 14:06:06 +0200

    restored points

--- src/drawer/points.js
+++ src/drawer/points.js
@@ -1,6 +1,6 @@
 import { canvas, getContext, center, Point } from "../canvas";
 import { calculate } from "circumcenter-calculator";
-import combinations from "../util";
+import combinations from 'ml-combinations';
 
 export const SCALE = 10;
 
@@ -10,29 +10,42 @@ const CIRCUMCENTER_CONTEXT = "points";
 function drawPointFromContext(ctx, point, diameter = 3) {
   const actualX = center().x + point.x * SCALE;
   const actualY = center().y - point.y * SCALE;
-
+  
   if (actualX > canvas().width || actualY > canvas().height) {
     throw `Point ${point.toString()} out of border`;
   }
-
+  
   ctx.beginPath();
   ctx.arc(actualX, actualY, diameter, 0, 2 * Math.PI, true);
   ctx.fill();
-
+  
   return point;
 }
 
 function drawCircumcenters(points, opacity = 9) {
-  const centers = combinations(points).map(points => {
-    return calculate(...points);
-  });
-
-  return centers.length > 2 ? drawCircumcenters(centers, ++opacity) : [];
+  if(points.length < 3) {
+    return;
+  }
+  
+  const circumcenterCtx = getContext(CIRCUMCENTER_CONTEXT);
+  const combinationsIterator = combinations(3, points.length, {mode: 'index'});
+  
+  for(let combination of combinationsIterator) {
+    drawPointFromContext(
+      circumcenterCtx,
+      calculate(
+        points[combination[0]],
+        points[combination[1]],
+        points[combination[2]]
+      ),
+      opacity
+    );
+  }
 }
 
 export function drawPoints(points) {
   const pointCtx = getContext(POINTS_CONTEXT);
-
+  
   points.forEach(point => {
     if (!point.isDrawn) {
       pointCtx.fillStyle = "green";
@@ -40,6 +53,6 @@ export function drawPoints(points) {
       point.isDrawn = true;
     }
   });
-
+  
   drawCircumcenters(points);
 }
